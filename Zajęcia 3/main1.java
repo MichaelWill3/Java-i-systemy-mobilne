934cc301731995e59250a2f284d5544c27632d1a
MichaelWill3 <michaelwill3@gmail.com>
2019-03-26 20:24:23 +0100

    Add files via upload

--- Zajęcia 3/main1.java
+++ Zajęcia 3/main1.java
@@ -1,12 +1,65 @@
-import java.util.Iterator;
 
+class Matrix<T>{
+    private T[][] matrix;
+    private int columns;
+    private int rows;
+
+    public Matrix(T[][] _matrix)
+    {
+        matrix = _matrix;
+        columns = matrix.length;
+        rows = matrix[0].length;
+    }
+
+    public T get(int column, int row)
+    {
+        return matrix[column][row];
+    }
+
+    public int columns(){
+        return columns;
+    }
+    public int rows(){
+        return rows;
+    }
+
+    public void add(Matrix<T> _matrix)
+    {
+        T type =_matrix.get(0,0);
+        if(type  instanceof String ) {
+            if(_matrix.rows() != this.rows || _matrix.columns() != this.columns) {
+                System.out.println("you cant do that ):");
+                return;
+            }
+            //System.out.println("to je string");
+            for(int i = 0; i < _matrix.columns(); i++)
+            {
+                for(int j = 0; j < _matrix.columns(); j++)
+                {
+                    //this.matrix[i][j] = matrix[i][j] + _matrix[i][j];
+                }
+            }
+        }
+        else
+        {
+            for(int i = 0; i < _matrix.columns(); i++)
+            {
+                for(int j = 0; j < _matrix.columns(); j++)
+                {
+                    //this.matrix[i][j] = Double.parseDouble(matrix[i][j] ) + _matrix[i][j];
+                }
+            }
+        }
+    }
+}
 public class main1 {
 
-
-
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args){
+        String[][] string1 = {{"a", "b"}};
+        String[][] string2 = {{"k", "c"}};
+        Matrix<String> M1 = new Matrix<String>(string1);
+        Matrix<String> M2 = new Matrix<String>(string2);
+        M1.add(M2);
     }
 
 }
--- Zajęcia 3/main3_2.java
+++ Zajęcia 3/main3_2.java
@@ -1,107 +1,64 @@
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
-class Substring
-{
-    int substring(String a, String b)
-    {
-        ArrayList<Integer> InitialSearchPoints = new ArrayList<Integer>();
-        int out_;
-        boolean FoundBInA = false;
-        for(int i = 0; i < b.length(); i++)
-        {
-            if(b.charAt(i) == a.charAt(0))
-            {
-                FoundBInA = true;
-                InitialSearchPoints.add(i);
-            }
-        }
-
-        if(FoundBInA == false)
-            return -1;
-        boolean AddPrefix;
-        boolean AddSuffix;
-
-        for(Integer StartPoint : InitialSearchPoints)
-        {
-            boolean ok = true;
-            if(StartPoint == 0)
-            {
-                AddPrefix = false;
-            }
-            else
-            {
-                AddPrefix = true;
-            }
-            for(int i = 0; i < a.length(); i++)
-            {
-                if(i != a.length())
-                {
-
-                }
-                ok = true;
-                if(b.charAt((StartPoint + i)%b.length()) != a.charAt(i))
-                {
-                    ok = false;
-                }
-            }
-            if(ok)
-            {
-                return out_;
-            }
-        }
-    }
-}
 public class main3_2{
-    //ArrayList<Integer> InitialSearchPoints = new ArrayList<Integer>();
     static int substring(String a, String b)
     {
-        ArrayList<Integer> InitialSearchPoints = new ArrayList<Integer>();
-        int out_ = a.length()/b.length();
-        boolean FoundBInA = false;
-        int InitialPosition;
-        for(InitialPosition = 0; InitialPosition < b.length(); InitialPosition++)
+        //b contains a
+        int out_ = -1;
+        boolean FOUNDaINb = false;
+        ArrayList<Integer> SearchStartingPoints = new ArrayList();
+        ArrayList<Integer> out = new ArrayList();
+        for(int i = 0; i < a.length(); i++)
         {
-            if(b.charAt(InitialPosition) == a.charAt(0))
+            if(a.charAt(i) == b.charAt(0))
             {
-                //if(InitialPosition != 0)
-                //    out_ = out_ + 1;
-                FoundBInA = true;
-                InitialSearchPoints.add(InitialPosition);
-                //break;
-
+                SearchStartingPoints.add(i);
+                FOUNDaINb = true;
             }
         }
-        if(!FoundBInA)
-            return -1;
-        for(Integer InitPos : InitialSearchPoints)
+
+        int TheRightStartingPointThatDoesNotLetMeDownEvenThoughItCan = -1;
+        for(int StartingPoint : SearchStartingPoints)
         {
-            boolean ok = false;
-            int i = InitPos;
-            for(int j = 0; i < a.length(); i++, j++)
+            boolean ok = true;
+            for(int i = 0; i < b.length(); i++)
             {
-                ok = true;
-                if(a.charAt(j) != b.charAt(i % b.length())) {
+                if(b.charAt(i) != a.charAt((i+StartingPoint)%a.length()))
+                {
                     ok = false;
-                    break;
                 }
             }
-            //if(i % b.length() != b.length())
-            //    out_ = out_ + 1;
             if(ok)
             {
-                return out_;
+                TheRightStartingPointThatDoesNotLetMeDownEvenThoughItCan = StartingPoint;
+                break;
             }
-
         }
-        return -1;
+        if(TheRightStartingPointThatDoesNotLetMeDownEvenThoughItCan == -1)
+            return -1;
+
+        boolean prefix = false;
+        boolean suffix = false;
+        if(TheRightStartingPointThatDoesNotLetMeDownEvenThoughItCan != 0)
+            prefix = true;
+        String tmp = b.substring(TheRightStartingPointThatDoesNotLetMeDownEvenThoughItCan);
+        if(tmp.length()/a.length() == (int)tmp.length()/a.length())
+            suffix = true;
+
+        out_ = (int)tmp.length()/a.length();
+
+        if(suffix)
+            out_ = out_ + 1;
+        if(prefix)
+            out_ = out_ + 1;
+
+        return out_;
     }
 
 
     public static void main(String[] args){
-        String a = "asananasananasananasanan";
-        String b = "ananas";
+        String a = "ananas";
+        String b = "asananasananasan";
         System.out.println(substring(a,b));
     }
 }
